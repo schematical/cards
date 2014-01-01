af674cd9a93bc2f620554613f663f93c51ff5516
Matt Lea <mlea@schematical.com>
2014-01-01 15:16:04 -0600

    updating

--- cards/www/js/controllers.js
+++ cards/www/js/controllers.js
@@ -39,91 +39,47 @@ angular.module('starter.controllers', [])//angular.module('sideMenuTest', ['ioni
     // Main app controller, empty for the example
     $scope.cards = Cards.shuffle();
 
-    var startApp = function() {
-        $location.path('/main');
 
-        // Set a flag that we finished the tutorial
-        window.localStorage['didTutorial'] = true;
-    };
-
-    // Check if the user already did the tutorial and skip it if so
-    if(window.localStorage['didTutorial'] === "true") {
-        startApp();
-        return;
-    }
 
     // Move to the next slide
     $scope.next = function() {
         $scope.$broadcast('slideBox.nextSlide');
     };
+    // Move to the next slide
+    $scope.prev = function() {
+        $scope.$broadcast('slideBox.prevSlide');
+    };
 
-
-    // Our initial right buttons
-    var rightButtons = [
-        {
-            content: 'Next',
-            type: 'button-positive button-clear',
-            tap: function(e) {
-                // Go to the next slide on tap
-                $scope.next();
+        // If this is not the first slide, give it a back button
+        $scope.leftButtons = [
+            {
+                content: 'Back',
+                type: 'button-positive button-clear',
+                tap: function(e) {
+                    // Move to the previous slide
+                    $scope.prev();
+                }
             }
-        }
-    ];
+        ];
 
-    // Our initial left buttons
-    var leftButtons = [
-        {
-            content: 'Skip',
-            type: 'button-positive button-clear',
-            tap: function(e) {
-                // Start the app on tap
-                startApp();
+
+        $scope.rightButtons = [
+            {
+                content: 'Next',
+                type: 'button-positive button-clear',
+                tap: function(e) {
+                    $scope.next();
+                }
             }
-        }
-    ];
-
-    // Bind the left and right buttons to the scope
-    $scope.leftButtons = leftButtons;
-    $scope.rightButtons = rightButtons;
+        ];
 
 
     // Called each time the slide changes
     $scope.slideChanged = function(index) {
 
-        // Check if we should update the left buttons
-        if(index > 0) {
-            // If this is not the first slide, give it a back button
-            $scope.leftButtons = [
-                {
-                    content: 'Back',
-                    type: 'button-positive button-clear',
-                    tap: function(e) {
-                        // Move to the previous slide
-                        $scope.$broadcast('slideBox.prevSlide');
-                    }
-                }
-            ];
-        } else {
-            // This is the first slide, use the default left buttons
-            $scope.leftButtons = leftButtons;
-        }
 
-        // If this is the last slide, set the right button to
-        // move to the app
-        if(index == 2) {
-            $scope.rightButtons = [
-                {
-                    content: 'Start using MyApp',
-                    type: 'button-positive button-clear',
-                    tap: function(e) {
-                        startApp();
-                    }
-                }
-            ];
-        } else {
-            // Otherwise, use the default buttons
-            $scope.rightButtons = rightButtons;
-        }
+
+
     };
 
 
